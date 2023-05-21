24b9b1e7d030c40db5b943ca060b2cdfc30013b2
Martin Alvarez <martin.alvarez@triplemint.com>
2023-05-21 11:41:15 -0300

    Add video models and update videos page

---
+++ next-page-router-curator/src/models/videos.js
@@ -0,0 +1,42 @@
+const videos = [
+    {
+        videoId: 'PVKF3vApBfo',
+    },
+    {
+        videoId: 'J8ArEhg3JR0',
+    },    
+    {
+        videoId: '5hyCyXO8hiQ',
+    },    
+    {
+        videoId: '_kkWwUBxFCM',
+    },    
+    {
+        videoId: '01Zk-hpiqSo',
+    },    
+    {
+        videoId: '4oL-mdPQEtM',
+    },    
+    {
+        videoId: 'ty9FgvkgsW8',
+    },    
+    {
+        videoId: 'LDQgj23tIJw',
+    },    
+    {
+        videoId: 'fUauc_38Fxs',
+    },    
+    {
+        videoId: 'WV2aKtsNQ50',
+    },    
+    {
+        videoId: 't0I9ygFbFHo',
+    },    
+    {
+        videoId: '0FQ3Ucml458',
+    },    
+];
+
+export function getAllVideos() {
+    return videos;
+}
--- next-page-router-curator/src/pages/videos/index.js
+++ next-page-router-curator/src/pages/videos/index.js
@@ -1,8 +1,26 @@
+import { getAllVideos } from "@/models/videos";
+
 function Videos() {
+    const videos = getAllVideos();
     return(
         <>
             <h1>Videos Page</h1>
-            <p>Coming soon</p>
+
+            {videos.map(({ videoId }) => {
+                return (
+                    <iframe
+                        key={videoId}
+                        width="100%"
+                        height="100%"    
+                        src={`https://www.youtube.com/embed/${videoId}`}
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        allowFullScreen>
+                    </iframe> 
+                );
+            })}
+
         </>
     );
 }
